8f649e3e1a947445e791be034fc1f73e45c06477
romanb123 <romanb123@users.noreply.github.com>
2019-02-16 15:50:15 +0200

    upd

--- bootstrap-3.3.7-dist/js/slider.js
+++ bootstrap-3.3.7-dist/js/slider.js
@@ -1,25 +1,26 @@
 
 function geturl() {
     var newurl = document.getElementById('inside').value;
-    var fatherofdiv = document.getElementById('fatherc');
-    var childdiv = document.createElement('div'); 
-    childdiv.setAttribute("class","item")
-    var newpic = document.createElement("img");
-    newpic.setAttribute("src",newurl); 
-    newpic.setAttribute("width","100%");
-    newpic.style.height="300px";
-    console.log(newpic);
-    childdiv.appendChild(newpic);
-    console.log(childdiv);
-    fatherofdiv.appendChild(childdiv);
-    console.log(fatherofdiv);
-
     var childdiv2 = document.createElement('div');
     var pic2=document.createElement("img");
     childdiv2.setAttribute("class","col-lg-1 col-md-3 col-sm-3 col-xs-6  glr");
    childdiv2.appendChild(pic2);
     pic2.setAttribute("src", newurl); 
+    pic2.setAttribute("data-toggle","modal");
+    pic2.setAttribute("data-target",".bs-example-modal-sm");
     pic2.style.height='100px';
     pic2.style.width='100%';
     document.getElementById("therow").appendChild(childdiv2);
+    pic2.addEventListener("click", function(event) {
+    console.log(event.target.src);
+    document.getElementById("modalpic").src=event.target.src;
+    document.getElementById("modalpic").style.width="400px";
+    document.getElementById("modalpic").style.height="400px";
+      });
 }
+
+
+   
+    
+
+
