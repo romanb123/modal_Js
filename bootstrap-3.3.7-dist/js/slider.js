16ba31dff052ca0b440776294ebba7408ee30d5d
romanb123 <romanb123@users.noreply.github.com>
2019-02-16 16:42:19 +0200

    upd

--- bootstrap-3.3.7-dist/js/slider.js
+++ bootstrap-3.3.7-dist/js/slider.js
@@ -7,15 +7,15 @@ function geturl() {
    childdiv2.appendChild(pic2);
     pic2.setAttribute("src", newurl); 
     pic2.setAttribute("data-toggle","modal");
-    pic2.setAttribute("data-target",".bs-example-modal-sm");
+    pic2.setAttribute("data-target",".bs-example-modal-lg");
     pic2.style.height='100px';
     pic2.style.width='100%';
     document.getElementById("therow").appendChild(childdiv2);
     pic2.addEventListener("click", function(event) {
     console.log(event.target.src);
     document.getElementById("modalpic").src=event.target.src;
-    document.getElementById("modalpic").style.width="400px";
-    document.getElementById("modalpic").style.height="400px";
+    document.getElementById("modalpic").style.width="600px";
+    document.getElementById("modalpic").style.height="300px";
       });
 }
 
